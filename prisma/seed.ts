23eb5a20c05bb1745fae36fb7c88ab64e3238220
domino4820 <thathayho91@gmail.com>
2025-11-27 11:24:45 +0700

    chore: migrate seed config to v7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import prisma from '@/utils/prisma.js';
 import { hash } from 'bcrypt';
-
-const prisma = new PrismaClient();
 
 const main = async () => {
     const hashedPassword = await hash('admin', 10);
-    await prisma.admin.upsert({
+    const admin = await prisma.admin.upsert({
         where: { username: 'admin' },
         update: {
             password: hashedPassword
@@ -15,11 +13,13 @@ const main = async () => {
             password: hashedPassword
         }
     });
+    console.log({ admin });
 };
 
 try {
     await main();
-} catch {
+} catch (e) {
+    console.error(e);
     process.exit(1);
 } finally {
     await prisma.$disconnect();
